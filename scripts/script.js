39c0b9dccc7407841c6fa3a36de47a552c8e93b9
Sabri Ozdil <sabriozdil07@gmail.com>
2023-12-08 09:53:06 +0100

    voortgang 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+
+const zoekButton = document.querySelector("header > button");
+const zoekSection = document.querySelector("header > search");
+
+zoekButton.onclick = toggleSearch;
+
+function toggleSearch() {
+	zoekSection.classList.toggle("open");
+}
